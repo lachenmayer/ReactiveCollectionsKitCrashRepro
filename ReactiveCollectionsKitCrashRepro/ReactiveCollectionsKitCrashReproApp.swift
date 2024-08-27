cc984f9bbb60829388d972e00a59e0291f45b67a
harry lachenmayer <harrylachenmayer@gmail.com>
2024-08-27 14:57:32 +0100

    Initial Commit

---
+++ ReactiveCollectionsKitCrashRepro/ReactiveCollectionsKitCrashReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactiveCollectionsKitCrashReproApp.swift
+//  ReactiveCollectionsKitCrashRepro
+//
+//  Created by Harry Lachenmayer on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReactiveCollectionsKitCrashReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
